d6f5058486c2c1f9e0625e679183747b00713356
Martijn Pieterse <pieterse@gmail.com>
2013-07-31 19:45:13 +0200

    Figuring out how pygtk/glade works..

--- nut.py
+++ nut.py
@@ -33,47 +33,45 @@ class Handler:
    def onDeleteWindow(self, *args):
       gtk.main_quit(*args)
 
-   def onButton1Pressed(self, button):
-      print "Hello World!"
-
-   def onButton2Pressed(self, button):
-      print "Destroy The World!"
-
-   def menu_quit_action(self, menu):
-      print "menu Quit"
-      gtk.main_quit()
-      
    def mySearch(self, entry):
-      print "x"
-      t = builder.get_object("treeview2")
+      print "Entry contains: ", entry.get_text()
 
-      l = gtk.ListStore(str)
+      print t.get_cursor()
+      print t.get_cursor().get_title()
 
-      l.append(["MAPI"])
-      t.set_model(l)
-   
+
 builder = gtk.Builder()
 builder.add_from_file("nut.glade")
 builder.connect_signals(Handler())
 
-t = builder.get_object("treeview2")
+t = builder.get_object("treeview1")
 
 liststore = gtk.ListStore(str)
-        
-liststore.append(["Ubuntu"])
-liststore.append(["Fedora"])
-liststore.append(["Sabayon"])
-liststore.append(["Arch"])
-liststore.append(["Debian"])
+
+liststore.append(["1"])
+liststore.append(["HUBBLe"])
+liststore.append(["MARS1"])
+liststore.append(["MARS2"])
+liststore.append(["MARS3"])
+liststore.append(["MARS4"])
+liststore.append(["MARS5"])
+liststore.append(["MARS6"])
+liststore.append(["MARS7"])
+liststore.append(["MARS8"])
+liststore.append(["MARS9 maar dan een hele lang die eigenlijk niet op het scherm past, maar nu nog wel omdat de window scaled..."])
+liststore.append(["MARS9"])
+liststore.append(["MARS0"])
+liststore.append(["m5"])
+for i in range(10):
+    liststore.append(["planet%d"%(i)])
         
 t.set_model(liststore)
-column = gtk.TreeViewColumn("Selected.. No.. found..")
+column = gtk.TreeViewColumn("Matched foods:")
 t.append_column(column)
         
 cell = gtk.CellRendererText()
 column.pack_start(cell, False)
 column.add_attribute(cell, "text", 0)
-
 
 window = builder.get_object("window1")
 window.show_all()
