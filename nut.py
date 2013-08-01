4235f52d5ecdc91575ac4bfe94b656b41b2c5208
Martijn Pieterse <pieterse@gmail.com>
2013-08-01 20:13:17 +0200

    Getting somewhere..

--- nut.py
+++ nut.py
@@ -5,14 +5,13 @@ con = sqlite3.connect("/home/pieterse/Compile/nut_tcltk/nut.sqlite")
 
 cur = con.cursor()
 
-cur.execute('SELECT NDB_No,Long_Desc FROM food_des WHERE NDB_No>30060')
+cur.execute('SELECT NDB_No,Long_Desc FROM food_des WHERE Long_Desc LIKE "%kol%"');
 
 while True:
     data = cur.fetchone()
 
     if data == None: break
-    print "Data: ", data
-
+    print "Data: [", data, "]", data[0], data[1]
 
 
 import sys
@@ -30,14 +29,49 @@ except:
 
 
 class Handler:
-   def onDeleteWindow(self, *args):
-      gtk.main_quit(*args)
+    def onDeleteWindow(self, *args):
+        gtk.main_quit(*args)
 
-   def mySearch(self, entry):
-      print "Entry contains: ", entry.get_text()
+    def mySearch(self, entry):
+        e = entry.get_text()
+        print "Entry contains: ", e
+        liststore.clear()
+        
+        if len(e) >= 3:
+            sql = "SELECT NDB_No,Long_Desc FROM food_des WHERE Long_Desc LIKE \"%" + entry.get_text() + "%\""
 
-      print t.get_cursor()
-      print t.get_cursor().get_title()
+            print sql
+            cur.execute(sql)
+
+            while True:
+                data = cur.fetchone()
+
+                if data == None: break
+                liststore.append([data[0], data[1]])
+                
+
+def onSelectionChanged(a):
+    print a
+
+    (model, pathlist) = tree_selection.get_selected_rows()
+    for path in pathlist :
+        print "a"
+        tree_iter = model.get_iter(path)
+        value0 = model.get_value(tree_iter,0)
+        print value0
+        value1 = model.get_value(tree_iter,1)
+        print value1       
+
+        sql = "SELECT CHOCDF FROM food_des WHERE NDB_No=%s" % (value0)
+        cur.execute(sql)
+        
+        data = cur.fetchone()
+
+        value = data[0]
+        e = builder.get_object("CHOCDF")
+        print e
+
+        e.set_text(repr(value))
 
 
 builder = gtk.Builder()
@@ -46,32 +80,23 @@ builder.connect_signals(Handler())
 
 t = builder.get_object("treeview1")
 
-liststore = gtk.ListStore(str)
+liststore = gtk.ListStore(int, str)
 
-liststore.append(["1"])
-liststore.append(["HUBBLe"])
-liststore.append(["MARS1"])
-liststore.append(["MARS2"])
-liststore.append(["MARS3"])
-liststore.append(["MARS4"])
-liststore.append(["MARS5"])
-liststore.append(["MARS6"])
-liststore.append(["MARS7"])
-liststore.append(["MARS8"])
-liststore.append(["MARS9 maar dan een hele lang die eigenlijk niet op het scherm past, maar nu nog wel omdat de window scaled..."])
-liststore.append(["MARS9"])
-liststore.append(["MARS0"])
-liststore.append(["m5"])
-for i in range(10):
-    liststore.append(["planet%d"%(i)])
-        
 t.set_model(liststore)
-column = gtk.TreeViewColumn("Matched foods:")
-t.append_column(column)
-        
+column1 = gtk.TreeViewColumn("Hidden:")
+t.append_column(column1)
+column2 = gtk.TreeViewColumn("Matched foods:")
+t.append_column(column2)
+
 cell = gtk.CellRendererText()
-column.pack_start(cell, False)
-column.add_attribute(cell, "text", 0)
+
+column1.pack_start(cell, False)
+column1.add_attribute(cell, "text", 0)
+column2.pack_start(cell, False)
+column2.add_attribute(cell, "text", 1)
+
+tree_selection = t.get_selection()      
+tree_selection.connect("changed", onSelectionChanged)
 
 window = builder.get_object("window1")
 window.show_all()
